3570bb793f51013b2f77ede0a13e4cbfdda555b4
FlorGubiotti01 <flor.gubiotti@alumnos.frm.utn.edu.ar>
2023-05-23 19:48:02 -0300

    Se agregan metodos que seleccionan un ganador en la clase Poker

--- pythonPoker/main.py
+++ pythonPoker/main.py
@@ -6,6 +6,5 @@ if __name__ == '__main__':
 
     poker = Poker()  # Se crea un objeto de la clase Poker
 
-    #poker.iniciarJuego()
-    #poker.evaluarManos(poker.jugadorReal)  # Le paso como parametro un jugador para que evalue su mano
-    poker.iniciarJuegoCompleto();
+    poker.iniciarJuegoCompleto()
+

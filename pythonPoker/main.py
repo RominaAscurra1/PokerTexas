f1144a7e2ec92f19a87ca18c7f659d8904387d5e
RominaAscurra1 <romina.ascurra@alumnos.frm.utn.edu.ar>
2023-05-23 20:40:56 +0200

    Llamada al mètodo iniciarJuegoCompleto de la clase poker

--- pythonPoker/main.py
+++ pythonPoker/main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
 
     poker = Poker()  # Se crea un objeto de la clase Poker
 
-    poker.iniciarJuego()
-    poker.evaluarManos(poker.jugadorReal)  # Le paso como parametro un jugador para que evalue su mano
-
+    #poker.iniciarJuego()
+    #poker.evaluarManos(poker.jugadorReal)  # Le paso como parametro un jugador para que evalue su mano
+    poker.iniciarJuegoCompleto();
